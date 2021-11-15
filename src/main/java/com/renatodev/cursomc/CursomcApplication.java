2bfd964722fb75dc8b95cbf60fe7c445f962d8bb
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2021-11-15 12:03:05 -0300

    Estado e cidade

--- src/main/java/com/renatodev/cursomc/CursomcApplication.java
+++ src/main/java/com/renatodev/cursomc/CursomcApplication.java
@@ -1,8 +1,12 @@
 package com.renatodev.cursomc;
 
 import com.renatodev.cursomc.domain.Categoria;
+import com.renatodev.cursomc.domain.Cidade;
+import com.renatodev.cursomc.domain.Estado;
 import com.renatodev.cursomc.domain.Produto;
 import com.renatodev.cursomc.repositories.CategoriaRepository;
+import com.renatodev.cursomc.repositories.CidadeRepository;
+import com.renatodev.cursomc.repositories.EstadoRepository;
 import com.renatodev.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,33 +19,52 @@ import java.util.Collections;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
-	@Autowired
-	private ProdutoRepository produtoRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
-	}
+    @Autowired
+    private EstadoRepository estadoRepository;
 
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
+    @Autowired
+    private CidadeRepository cidadeRepository;
 
-		Produto p1 = new Produto(null, "Computador", 2000.00);
-		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Mouse", 80.00);
+    public static void main(String[] args) {
+        SpringApplication.run(CursomcApplication.class, args);
+    }
 
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Collections.singletonList(p2));
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
 
-		p1.getCategorias().addAll(Collections.singletonList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Collections.singletonList(cat1));
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
 
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-	}
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Collections.singletonList(p2));
+
+        p1.getCategorias().addAll(Collections.singletonList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Collections.singletonList(cat1));
+
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+        est1.getCidades().addAll(Collections.singletonList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+    }
 }
---
+++ src/main/java/com/renatodev/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.renatodev.cursomc.repositories;
+
+import com.renatodev.cursomc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+}
