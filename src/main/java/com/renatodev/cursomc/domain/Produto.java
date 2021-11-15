c2e4e7b8c5e7f47382831248c773e5c0cf0af288
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2021-11-15 16:29:16 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/renatodev/cursomc/domain/Produto.java
+++ src/main/java/com/renatodev/cursomc/domain/Produto.java
@@ -1,8 +1,6 @@
 package com.renatodev.cursomc.domain;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
---
+++ src/main/java/com/renatodev/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.renatodev.cursomc.repositories;
+
+import com.renatodev.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+}
