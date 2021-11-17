6cb296838d143a08576ce4e7ce2533ab9be1cc51
Renato Pereira Santos <renatodev95@gmail.com>
2021-11-17 18:09:01 -0300

    Inserindo uma nova Categoria com @PostMapping

--- src/main/java/com/renatodev/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/renatodev/cursomc/resources/CategoriaResource.java
@@ -4,10 +4,10 @@ import com.renatodev.cursomc.domain.Categoria;
 import com.renatodev.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/categorias")
@@ -20,5 +20,16 @@ public class CategoriaResource {
     public ResponseEntity<?> find(@PathVariable Long id) {
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+        categoria = service.insert(categoria);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(categoria.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
