cd27056b783713227c914e86b66c9707c124a4b9
Renato Pereira Santos <renatodev95@gmail.com>
2021-12-04 12:03:59 -0300

    removing unnecessary method

--- src/main/java/com/renatodev/cursomc/CursomcApplication.java
+++ src/main/java/com/renatodev/cursomc/CursomcApplication.java
@@ -1,19 +1,13 @@
 package com.renatodev.cursomc;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner {
+public class CursomcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
     }
 
 }
