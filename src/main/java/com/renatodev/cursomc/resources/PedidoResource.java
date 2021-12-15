dc702d3e1876bf9301b4e4498b4a2cd8818f687c
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2021-12-15 11:55:56 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/renatodev/cursomc/resources/PedidoResource.java
+++ src/main/java/com/renatodev/cursomc/resources/PedidoResource.java
@@ -1,10 +1,9 @@
 package com.renatodev.cursomc.resources;
 
-import com.renatodev.cursomc.domain.Categoria;
 import com.renatodev.cursomc.domain.Pedido;
-import com.renatodev.cursomc.dto.CategoriaDTO;
 import com.renatodev.cursomc.services.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -34,5 +33,15 @@ public class PedidoResource {
                 .buildAndExpand(pedido.getId())
                 .toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Pedido>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "instante") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+        Page<Pedido> list = service.findPage(page, linesPerPage, orderBy, direction);
+        return ResponseEntity.ok(list);
     }
 }
--- src/main/java/com/renatodev/cursomc/services/PedidoService.java
+++ src/main/java/com/renatodev/cursomc/services/PedidoService.java
@@ -8,9 +8,13 @@ import com.renatodev.cursomc.domain.enums.EstadoPagamento;
 import com.renatodev.cursomc.repositories.ItemPedidoRepository;
 import com.renatodev.cursomc.repositories.PagamentoRepository;
 import com.renatodev.cursomc.repositories.PedidoRepository;
-import com.renatodev.cursomc.repositories.ProdutoRepository;
+import com.renatodev.cursomc.security.UserSS;
+import com.renatodev.cursomc.services.exceptions.AuthorizationException;
 import com.renatodev.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -67,5 +71,15 @@ public class PedidoService {
         itemPedidoRepository.saveAll(pedido.getItens());
         emailService.sendOrderConfirmationHtmlEmail(pedido);
         return pedido;
+    }
+
+    public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        UserSS user = UserService.authenticated();
+        if (user == null) {
+            throw new AuthorizationException("Acesso negado");
+        }
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Cliente cliente =  clienteService.find(user.getId());
+        return pedidoRepository.findByCliente(cliente, pageRequest);
     }
 }
